dbbc9916f3c9d661821624d0ef714045c8c14962
Sergo <rogaldh@radsh.red>
2025-10-31 10:01:42 +0000

    fix: allow to display interactive tab

--- app/address/[address]/idl/page-client.tsx
+++ app/address/[address]/idl/page-client.tsx
@@ -4,10 +4,11 @@ import { LoadingCard } from '@components/common/LoadingCard';
 import { Suspense } from 'react';
 
 import { IdlCard } from '@/app/components/account/idl/IdlCard';
+import { IdlCard as InteractiveIdlCard } from '@/app/features/idl';
 
-// import { IdlCard } from '@/app/features/idl';
 import { PageRenderer } from './page-renderer';
 
+const isInteractiveIdlEnabled = process.env.NEXT_PUBLIC_INTERACTIVE_IDL_ENABLED === 'true';
 export default function IdlPageClient({ address }: Readonly<{ address: string }>) {
     return <PageRenderer address={address} renderComponent={IdlRenderComponent} />;
 }
@@ -15,7 +16,7 @@ export default function IdlPageClient({ address }: Readonly<{ address: string }>
 function IdlRenderComponent({ address }: { address: string }) {
     return (
         <Suspense fallback={<LoadingCard message="Loading program IDL" />}>
-            <IdlCard programId={address} />
+            {isInteractiveIdlEnabled ? <InteractiveIdlCard programId={address} /> : <IdlCard programId={address} />}
         </Suspense>
     );
 }
