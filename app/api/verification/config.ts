5c2c4a9dbec25d56fed8ebf1ad6b043826ae48f3
Tania Markina <tanyamarkina@list.ru>
2026-04-21 20:57:29 +0200

    build fix

--- app/api/verification/config.ts
+++ app/api/verification/config.ts
@@ -4,3 +4,7 @@ export const ERROR_CACHE_MAX_AGE = 30;
 export const CACHE_HEADERS = {
     'Cache-Control': `public, max-age=${CACHE_MAX_AGE}, s-maxage=${CACHE_MAX_AGE}, stale-while-revalidate=3600`,
 };
+
+export const NO_STORE_HEADERS = {
+    'Cache-Control': 'no-store',
+};
