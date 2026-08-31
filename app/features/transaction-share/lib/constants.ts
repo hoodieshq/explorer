618f31ee1e426af7727830aeb186489a7a91d3c8
Mikhail <mikhail@hoodies.company>
2026-08-31 15:35:48 +0300

    chore: remove process.env.VERCEL_PROJECT_PRODUCTION_URL from the  TX_OG_BASE_URL

--- app/features/transaction-share/lib/constants.ts
+++ app/features/transaction-share/lib/constants.ts
@@ -2,11 +2,8 @@ import { EXPLORER_BASE_URL } from '@utils/env';
 
 /**
  * Origin the share link and the OG image URL are built from.
- * Temporarily point to VERCEL_PROJECT_PRODUCTION_URL so we can test sharing.
  */
-export const TX_OG_BASE_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : EXPLORER_BASE_URL;
+export const TX_OG_BASE_URL = EXPLORER_BASE_URL;
 
 /**
  * Rows past this collapse into one "and N more" line, and only the programs above it get an IDL fetch -
