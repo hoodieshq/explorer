0d681a732229838e680b17613b748ec692106a97
Mikhail <mikhail@hoodies.company>
2026-09-17 16:47:22 +0300

    tmp: change base url to vercel for testing sharing on dev

--- app/features/transaction-share/env.ts
+++ app/features/transaction-share/env.ts
@@ -3,4 +3,8 @@ import { EXPLORER_BASE_URL } from '@utils/env';
 // Keep in env for a while as this one used to be env variable.
 export const isClusterProbeEnabled = false;
 
-export const TX_OG_BASE_URL = EXPLORER_BASE_URL;
+// temp change for testing sharing
+export const TX_OG_BASE_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : EXPLORER_BASE_URL;
+
